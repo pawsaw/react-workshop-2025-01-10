eb9f7b98603b6b77370c22d754bbcf187b966daa
Pawel Sawicki <pwlsawicki@gmail.com>
2025-01-13 09:02:26 +0100

    bookListItem

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import { AppHeader } from "./commponents/AppHeader";
-import { BookListItem } from "./commponents/BookListItem";
+import { BookListItem, BookListItemProps } from "./commponents/BookListItem";
 import { exampleBooks } from "./data/exampleBooks";
 import "./index.css";
 
 function App() {
+  const onBookClicked: BookListItemProps["onBookClicked"] = (clickedBook) => {
+    alert(clickedBook.price);
+  };
+
   return (
     <div>
       <AppHeader />
-      <BookListItem book={exampleBooks[0]} />
-      <BookListItem book={exampleBooks[1]} />
+      <BookListItem book={exampleBooks[0]} onBookClicked={onBookClicked} />
+      <BookListItem book={exampleBooks[1]} onBookClicked={onBookClicked} />
     </div>
   );
 }
--- src/commponents/BookListItem.tsx
+++ src/commponents/BookListItem.tsx
@@ -3,11 +3,15 @@ import { Book } from "../domain/book";
 
 export interface BookListItemProps {
   book: Book;
+  onBookClicked: (clickedBook: Book) => void;
 }
 
-export const BookListItem: React.FC<BookListItemProps> = ({ book }) => {
+export const BookListItem: React.FC<BookListItemProps> = ({
+  book,
+  onBookClicked,
+}) => {
   return (
-    <div className="example-book">
+    <div className="example-book" onClick={() => onBookClicked(book)}>
       <h2>{book.title}</h2>
       <h3>{book.subtitle}</h3>
       <div className="text-meta">by {book.author}</div>
